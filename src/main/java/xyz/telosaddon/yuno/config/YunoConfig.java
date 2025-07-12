f78ec16ccecaded0fa9510e2bebaafa0039d7b34
Erioooook <hoholoab@gmail.com>
2025-07-13 02:02:14 +0300

    Update YunoConfig.java

--- src/main/java/xyz/telosaddon/yuno/config/YunoConfig.java
+++ src/main/java/xyz/telosaddon/yuno/config/YunoConfig.java
@@ -4,27 +4,14 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
-@Config(name = "yuno")    // файл будет yuno.json в папке config
+@Config(name = "yuno")
 public class YunoConfig implements ConfigData {
-
+    
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 100)
     @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.BoundedDiscrete(min = 0, max = 20)
-    public double healthThreshold = 6.0;
+    public int healthThreshold = 6;
 
     @ConfigEntry.Gui.Tooltip
     public boolean autoDropEnabled = true;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import me.shedaniel.autoconfig.ConfigHolder;
-import xyz.telosaddon.yuno.config.YunoConfig;
-
-public class YunoClient implements ClientModInitializer {
-    public static ConfigHolder<YunoConfig> CONFIG;
-
-    @Override
-    public void onInitializeClient() {
-        CONFIG = AutoConfig.register(YunoConfig.class, GsonConfigSerializer::new);
-        // …твоя инициализация команд, миксинов и т.п.
-    }
-}
+}  // ← вот эта закрывающая скобка была потеряна
